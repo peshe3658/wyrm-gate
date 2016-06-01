074fbf793a00acd762b5cd306f2a52f8b06545c9
Tyrie7 <tyrie7@hotmail.com>
2016-06-01 15:33:17 +0000

    Add files via upload

--- Wyrm Gate/Assets/Scripts/PlayerController.cs
+++ Wyrm Gate/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
+	public float speed;
+	private Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb.AddForce (movement * speed);
 	}
 }
