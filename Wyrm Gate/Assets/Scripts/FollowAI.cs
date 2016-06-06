15ca0f25116696665e030f5f59e7bb53b03dcb93
peshe3658 <peter-9000@outlook.com>
2016-06-06 11:24:35 -0400

    Fixed Follow

--- Wyrm Gate/Assets/Scripts/FollowAI.cs
+++ Wyrm Gate/Assets/Scripts/FollowAI.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class FollowAI : MonoBehaviour {
 
-     private Rigidbody2D rb;
+    private Rigidbody2D rb;
     public float speed;
     // Use this for initialization
     void Start ()
@@ -19,7 +19,21 @@ public class FollowAI : MonoBehaviour {
         var player = GameObject.FindWithTag("Player");
         x = player.GetComponent<Rigidbody2D>().position.x;
         y = player.GetComponent<Rigidbody2D>().position.y;
-        Vector2 movement = new Vector2(-x, -y);
-        rb.AddForce(movement * speed);
+        Vector2 movement = new Vector2(x, y);
+        x = GetComponent<Rigidbody2D>().position.x;
+        y = GetComponent<Rigidbody2D>().position.y;
+        Vector2 current = new Vector2(x, y);
+        rb.AddForce(-1 * (Vector2.MoveTowards(current, movement, 2.0f) * speed));
+        if (rb.velocity.magnitude > 50 && rb.drag < 100)
+        {
+            rb.drag += 1.0f;
+        }
+
+        if (rb.velocity.magnitude < 50 && rb.drag > 0)
+        {
+            rb.drag -= 1.0f;
+        }
+
     }
+
 }
